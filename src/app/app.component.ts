a3df812d91c4ac4ec848812278dcc32a61653c1a
afolabiopakunle <opakunleafolabi@gmail.com>
2021-08-05 00:26:35 +0100

    Errors cleared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component, VERSION } from '@angular/core';
 
+export interface IComputer {
+  type: string,
+  department: string,
+  description: string,
+}
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name;
+  computers:[];
+  newServerName;
+  newServerContent;
 }
